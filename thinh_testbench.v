b697e6c50f10215ae79cec9c9bd2cd55f788cb8b
Thinh213 <thinhphuto@gmail.com>
2026-08-04 19:20:13 +0700

    Clean up comments in thinh_testbench.v
    Removed comments about signal declarations and types.

--- thinh_testbench.v
+++ thinh_testbench.v
@@ -1,10 +1,6 @@
 module tb_top_module;
 
-    // 1. Khai báo tín hiệu giả lập
-    // Đầu vào của mạch phải là 'reg' để có thể gán giá trị
     reg p1, p2, p3, p4, p5, p6, p7, p8, p9;
- 
-    // Đầu ra của mạch phải là 'wire' để nhận kết quả
     wire A, B, C, D, E;
     
     
